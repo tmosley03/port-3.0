290acd6ae9333a68ea423c27d273e8f7a750723f
unknown <tlm3dev@gmail.com>
2019-07-12 23:07:08 -0500

    sidebar clean up

--- src/assets/components/header/Header.js
+++ src/assets/components/header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 //import 'header/header.css';
 import './header.css';
-
+import name from './name.png'
 const header = () => {
     return(
         <div>
         <nav id= "header" className= "navbar nav-bar">
             {/* <h4>thomas mosley 3</h4> */}
-            <img src="../images/name.png" alt=""/>
+            <img src={ name } alt=""/>
             <div>
                 <h5>Web Developer | Strategic Partner</h5>
             </div>
--- src/assets/components/sidebar/Sidebar.js
+++ src/assets/components/sidebar/Sidebar.js
@@ -1,35 +1,36 @@
 import React from 'react';
-//import 'header/header.css';
+import './hover.css';
 import './sidebar.css';
 import headshot from './headshot.jpg'
 import logo from './logo.svg'
 
 const Sidebar = () => {
-return(
-<div>
-    <div id="sidebar-nav"
-         className="col-md-3">
-        <div id="sidebar-content">
-            <div id="nav-btns">
-            <img id="headshot" src={headshot} alt="" className="img-fluid" />
-            <p className="nav-text btn">About Me</p>
-           <p className="nav-text btn">Technology</p>
-           <p className="nav-text btn">Experience</p>
-           <p className="nav-text btn">Interests</p>
-           <p className="nav-text btn">Contact</p>
-            </div>
-            <br/>
-            <hr/>
-            <div className="footer">
-            <p className="sidebar-text">This site was brought to you by yours truly</p>
-          
-            <p className="sidebar-text">&</p>
-            <p className="sidebar-text">React JS <span><img src={ logo} alt=""  className="img-fluid App-logo"/></span></p>
+    return (
+        <div>
+            <div id="sidebar-nav"
+                className="col-md-3">
+                <div id="sidebar-content">
+                    <div id="nav-s">
+                        <img id="headshot" src={headshot} alt="" className="img-fluid" />
+                        <hr/>
+                        <p className="nav-text hvr-grow text-uppercase ">About Me</p>
+                        <p className="nav-text hvr-grow text-uppercase ">Technology</p>
+                        <p className="nav-text hvr-grow text-uppercase ">Experience</p>
+                        <p className="nav-text hvr-grow text-uppercase ">Interests</p>
+                        <p className="nav-text hvr-grow text-uppercase ">Contact</p>
+                    </div>
+                   
+                    <hr />
+                    <div className="footer">
+                        <p className="sidebar-text">This site was brought to you by yours truly</p>
+
+                        <p className="sidebar-text">&</p>
+                        <p className="sidebar-text">React JS <span><img src={logo} alt="" className="img-fluid App-logo" /></span></p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-)
+    )
 }
 
 export default Sidebar;
