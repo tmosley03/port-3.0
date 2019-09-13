adc2e5ed2ff893fd24c1e29e512944b7f4a828d1
mita <user@localhost.localdomain>
2019-09-13 12:59:48 -0500

    cleaning

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // components
 import Header from './assets/components/header/Header'
 import Sidebar from './assets/components/sidebar/Sidebar';
-import RSidebar from './assets/components/RSidebar/RSidebar';
+// import RSidebar from './assets/components/RSidebar/RSidebar';
 // import TempComp from './assets/components/temp-comp/TempComp';
 // pages
 import About from './assets/components/about/About'
@@ -10,7 +10,7 @@ import Contact from './assets/components/contact/Contact'
 import Experience from './assets/components/experience/Experience'
 import Interests from './assets/components/interests/Interests'
 import Technology from './assets/components/technology/Technology'
-import Home from './assets/components/home/Home'
+// import Home from './assets/components/home/Home'
 import Footer from './assets/components/footer/Footer'
 // dependencies
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -36,7 +36,7 @@ function App() {
               </div> */}
 
             <Switch>
-              <Route path="/" exact component={Home} />
+              <Route path="/" exact component={About} />
 
               <Route path="/about" component={About} />
               <Route path="/technology" component={Technology} />
