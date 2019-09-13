001f8d8012978e378b008ccf606c491707adc4f7
mita <user@localhost.localdomain>
2019-09-13 13:34:48 -0500

    added about copy

--- src/assets/components/about/About.js
+++ src/assets/components/about/About.js
@@ -15,12 +15,11 @@ function About() {
       <img id="about" src="https://media.giphy.com/media/3oEhnb9Jod4xU9Xsvm/giphy.gif" alt="" className="img-fluid" />
       <hr />
       <div id="about-text-div"className="col-lg-8 col-md-6 col-xs-4">
-      <p  className="text-justify">I am a Full Stack Web developer with a passion for problem solving. I am currently seeking a full time position as a Junior Front-End Developer. My ideal work situation would be to join a company with a strong development team that promotes continual learning and creative problem solving. With almost 10 years of sole proprietorship under my belt, I have had the opportunity to serve, partner with, and learn from a wide array of clientele including but not limited to, non-profits, small mom & pop business, large corporations, franchises, and individual consumers. </p>
-     
-      <p className="text-justify">
-      I began coding on my own in early 2017 and recently completed the Full Stack Web Development Bootcamp at Southern Methodist University in Dallas, TX. My goal is to draw from my experience as a small business owner and sole proprietor to identify and develop web based solutions that make life a little bit sweeter. I am more than confident that once placed on the right team, anything and everything becomes possible. If you have any questions or would like more information, please feel free to contact me via the links below. I look forward to building with you!.
-         <br />
-      -Thomas Mosley
+      <p  className="text-justify">
+      Thank you for taking the time to check out my site.  I am an experienced Full Stack Web Developer currently located in the Dallas Fort Worth area of north Texas, USA.  I travel whenever I get the chance and I’m always in search of that next bit of knowledge that forces me to approach things from a new angle.   I have trouble deciding weather jiu-jitsu or boxing is the most beautiful thing I’ve ever seen… or maybe it’s the rain forest? It depends on the day I guess.  If given the choice between Windows and Mac, I choose Linux.  As cliché’ as it is, I love coffee. Nothing fancy, just plain black coffee… good stuff.  Prince is my favorite artist of all time in any genre excluding no one. The guy was a genius.  I believe in taking chances.  Learning to to be comfortable in the midst of discomfort has proven to be one of the most valuable lessons life has bestowed upon me, without it I may never have fallen in love with programming.  I’m really, really, really okay at chess and will never turn down a match.  Experience is a dear teacher, but it is a fool that will learn from no other, so I tend to spend quite a bit of time consuming all sorts of tutorials on Udemy and Youtube whenever I find myself with some down time.  I take every encounter with a new person as an opportunity to learn something new, and perhaps should you need a developer for something you have in the pipeline, I’ll have the opportunity to meet with and learn something new and cool with you.
+      <br/><br/>
+
+- Thomas 
       </p>
       </div>
       <hr></hr>
