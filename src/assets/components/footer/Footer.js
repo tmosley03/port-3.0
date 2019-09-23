8f1f61059358b282a07810a1e896bff34a477cd7
mita <user@localhost.localdomain>
2019-09-22 20:31:49 -0500

    media query tech comp

--- src/assets/components/footer/Footer.js
+++ src/assets/components/footer/Footer.js
@@ -6,9 +6,9 @@ import './assets/css/Footer.css';
 function Footer() {
   return (
     <div id="footer" className="row navbar-nav fixed-bottom">
-        
+        <div className="container">
             <p className="text-center footer-txt">“This is the real secret of life — to be completely engaged with what you are doing in the here and now. <br/>And instead of calling it work, realize it is play.” – Alan Watts</p>
-              
+            </div>
     </div>
 
   );
