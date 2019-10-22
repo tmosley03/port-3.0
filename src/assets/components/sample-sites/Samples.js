f0ba48fd99a9e970e42289c28095c0dc3ad5621d
tmosley03 <tlm3dev@gmail.com>
2019-10-22 17:05:30 -0500

    example links

--- src/assets/components/sample-sites/Samples.js
+++ src/assets/components/sample-sites/Samples.js
@@ -19,14 +19,16 @@ function Samples() {
     <br />
     {/* 1 */}
     <div className='example-wrapper'>
-      <img src= {realpage} alt='screen-shot' className='site-screenshot' />
+      <a href='https://www.realpage.com' rel='noopener' target='blank'>
+      <img src= {realpage} alt='screen-shot' className='site-screenshot' /></a>
       <br />
       <p >RealPage.com</p>
       <p className=" example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
     </div>
      {/* 2 */}
     <div className='example-wrapper'>
-      <img src= {propertyware} alt='screen-shot' className='site-screenshot' />
+    <a href='https://www.propertyware.com' rel='noopener' target='blank'>
+      <img src= {propertyware} alt='screen-shot' className='site-screenshot' /></a>
       <br />
       <p >Propertyware.com</p>
       <p className=" example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
@@ -34,21 +36,24 @@ function Samples() {
 
     {/* 3 */}
     <div className='example-wrapper'>
-      <img src= {blutrend} alt='screen-shot' className='site-screenshot' />
+    <a href='http://blutrend.com' target='blank'>
+      <img src= {blutrend} alt='screen-shot' className='site-screenshot' /></a>
       <br />
       <p >BlueTrend.com</p>
       <p className="example-text text-justify">RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team..</p>
     </div>
     {/* 4 */}
     <div className='example-wrapper'>
-      <img src= {csa} alt='screen-shot' className='site-screenshot' />
+    <a href='http://csasecure.com'  rel='noopener' target='blank'>
+      <img src= {csa} alt='screen-shot' className='site-screenshot' /></a>
       <br />
       <p >CsaSecure.com</p>
       <p className="example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
     </div>
     {/* 5 */}
     <div className='example-wrapper'>
-      <img src= {mcd} alt='screen-shot' className='site-screenshot' />
+    <a href='http://jmcmcdonalds.com'  rel='noopener' target='blank'>
+      <img src= {mcd} alt='screen-shot' className='site-screenshot' /></a>
       <br />
       <p >JMC McDonald's Group</p>
       <p className="example-text text-justify">RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
