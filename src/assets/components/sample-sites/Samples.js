6dd973a7d485f48d5d7ca76b9585d973da5477cc
tmosley03 <tlm3dev@gmail.com>
2019-10-22 14:21:20 -0500

    edit samples

--- src/assets/components/sample-sites/Samples.js
+++ src/assets/components/sample-sites/Samples.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './assets/css/Samples.css';
-import csaDesktop from './assets/images/csaSecure_Desktop.png'
+import blutrend from './assets/images/blutrend.png'
+import propertyware from './assets/images/propertyware.png'
+import mcd from './assets/images/mcd.png'
+import realpage from './assets/images/realpage.png'
+import csa from './assets/images/csa.png'
 
 
 
@@ -15,14 +19,14 @@ function Samples() {
     <br />
     {/* 1 */}
     <div className='example-wrapper'>
-      <img src= {csaDesktop} alt='screen-shot' className='site-screenshot' />
+      <img src= {realpage} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >RealPage.com</p>
       <p className=" example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
     </div>
      {/* 2 */}
     <div className='example-wrapper'>
-      <img src= {csaDesktop} alt='screen-shot' className='site-screenshot' />
+      <img src= {propertyware} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >Propertyware.com</p>
       <p className=" example-text text-justify"> Propertyware.com serves as a portal for the Propertyware tool, a product offered by Real Page Inc. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
@@ -30,21 +34,21 @@ function Samples() {
 
     {/* 3 */}
     <div className='example-wrapper'>
-      <img src= {csaDesktop} alt='screen-shot' className='site-screenshot' />
+      <img src= {blutrend} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >BlueTrend.com</p>
       <p className="example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
     </div>
     {/* 4 */}
     <div className='example-wrapper'>
-      <img src= {csaDesktop} alt='screen-shot' className='site-screenshot' />
+      <img src= {csa} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >CsaSecure.com</p>
       <p className="example-text text-justify"> Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
     </div>
     {/* 5 */}
     <div className='example-wrapper'>
-      <img src= {csaDesktop} alt='screen-shot' className='site-screenshot' />
+      <img src= {mcd} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >JMC McDonald's Group</p>
       <p className="example-text text-justify"> Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
