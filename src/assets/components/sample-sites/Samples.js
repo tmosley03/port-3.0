38bd2686545b30f0d8797ab8db47024fb48a3a23
tmosley03 <tlm3dev@gmail.com>
2019-10-22 15:29:54 -0500

    sample sites

--- src/assets/components/sample-sites/Samples.js
+++ src/assets/components/sample-sites/Samples.js
@@ -12,7 +12,7 @@ import csa from './assets/images/csa.png'
 
 function Samples() {
   return (
-<div>
+<div className="col-lg-12">
 
     <h2>Site Expamples</h2>
     <br />
@@ -29,7 +29,7 @@ function Samples() {
       <img src= {propertyware} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >Propertyware.com</p>
-      <p className=" example-text text-justify"> Propertyware.com serves as a portal for the Propertyware tool, a product offered by Real Page Inc. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
+      <p className=" example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
     </div>
 
     {/* 3 */}
@@ -37,21 +37,21 @@ function Samples() {
       <img src= {blutrend} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >BlueTrend.com</p>
-      <p className="example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
+      <p className="example-text text-justify">RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team..</p>
     </div>
     {/* 4 */}
     <div className='example-wrapper'>
       <img src= {csa} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >CsaSecure.com</p>
-      <p className="example-text text-justify"> Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
+      <p className="example-text text-justify"> RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
     </div>
     {/* 5 */}
     <div className='example-wrapper'>
       <img src= {mcd} alt='screen-shot' className='site-screenshot' />
       <br />
       <p >JMC McDonald's Group</p>
-      <p className="example-text text-justify"> Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
+      <p className="example-text text-justify">RealPage.com is the flagship website for Real Page Inc.. I currently maintain, edit, and create new pages and functionality for this site as a part of a four person team.</p>
     </div>
 
 <hr />
